a8f661bc0d34924014e4ba7cf5bc20e38195e004
arkhipovau <arkhipovau@gmail.com>
2025-02-26 21:38:37 +0900

    Added missing HTML views for Contents

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,5 @@
-# Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-# end 
-
 Rails.application.routes.draw do
+  # Основные ресурсы
   resources :contents do
     resources :comments, only: [:create, :index]
     resources :likes, only: [:create, :destroy]
@@ -26,4 +12,13 @@ Rails.application.routes.draw do
 
   resources :users, only: [:create, :show]
   resources :moderators, only: [:create, :index]
+
+  # API namespace
+  namespace :api do
+    resources :contents, only: [:index, :show]
+  end
+
+  # Корневой маршрут (можно поменять контроллер и экшен)
+  root "contents#index"
 end
+
