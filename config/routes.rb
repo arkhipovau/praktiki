124fba90e125165954d5c9671fdf044f086009fd
arkhipovau <arkhipovau@gmail.com>
2025-03-05 20:17:18 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,26 @@
 Rails.application.routes.draw do
-  # Основные ресурсы
+  # Главная страница
+  root "pages#home"
+
+  # Ресурсы для контента
   resources :contents do
-    resources :comments, only: [:create, :index]
-    resources :likes, only: [:create, :destroy]
+    resources :comments, only: [:create, :index] # Вложенные маршруты для комментариев
+    resources :likes, only: [:create, :destroy]  # Вложенные маршруты для лайков
   end
 
+  # Отдельные ресурсы
   resources :categories
   resources :tags
   resources :notifications, only: [:index, :update]
   resources :analytics, only: [:index]
 
+  # Ресурсы пользователей
   resources :users, only: [:create, :show]
   resources :moderators, only: [:create, :index]
 
-  # API namespace
+  # API-пространство имён
   namespace :api do
     resources :contents, only: [:index, :show]
   end
-
-  # Корневой маршрут (можно поменять контроллер и экшен)
-  root "contents#index"
 end
 
